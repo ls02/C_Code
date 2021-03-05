1c69aefab3c46613eabdb0c2437db159add9649a
z189 <2877455773@qq.com>
2021-03-05 21:50:40 +0800

    2021

---
+++ test-3-5-1/test.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+//逆序字符串的函数
+void reverse(char* left, char* right)
+{
+    while (left < right)
+    {
+        char tmp = *left;
+        *left = *right;
+        *right = tmp;
+        left++;
+        right--;
+    }
+}
+
+int main()
+{
+    char arr[100] = { 0 };
+    //输入
+    gets_s(arr, 100);//读取一行
+    //处理
+    //abc def ghi
+    int len = strlen(arr);
+    //1. 逆序整个字符串
+    reverse(arr, arr + len - 1);
+    //2. 逆序每一个单词
+    char* start = arr;
+    char* end = arr;
+    while (*start)
+    {
+        end = start;
+        while (*end != ' ' && *end != '\0')
+        {
+            end++;
+        }
+        //逆序一个单词
+        reverse(start, end - 1);
+        if (*end == ' ')
+            start = end + 1;
+        else
+            start = end;
+    }
+
+    //输出
+    printf("%s\n", arr);
+
+    return 0;
+}
