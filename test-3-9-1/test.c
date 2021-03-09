17e0ca68d123bb9131e7709b4c68eb08e5b760a0
z189 <2877455773@qq.com>
2021-03-09 22:13:39 +0800

    2021

---
+++ test-3-9-1/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <limits.h>
+#include <malloc.h>
+#include <assert.h>
+
+int main()
+{
+	INT_MAX;
+
+	int ** ptr = (int**)calloc(12, sizeof(int));
+	
+
+
+	return 0;
+}
