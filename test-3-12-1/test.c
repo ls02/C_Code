312457820d23246f9523ce74fbc190b6ddba5873
z189 <2877455773@qq.com>
2021-03-13 21:18:03 +0800

    2021

---
+++ test-3-12-1/test.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int* p = NULL;
+
+	int *ptr = (int*)realloc(p, 40);
+	if (ptr)
+	{
+		p = ptr;
+	}
+	else
+	{
+		printf("ƒ⁄¥Ê…Í«Î ß∞‹!\n");
+
+		return 0;
+	}
+
+	return 0;
+}
+
+//int main()
+//{
+//	int* p = NULL;
+//	int i = 0;
+//
+//	int* ptr = (int*)calloc(40, 4);
+//
+//	if (ptr)
+//	{
+//		p = ptr;
+//	}
+//	else
+//	{
+//		printf("…Í«Îƒ⁄¥Ê ß∞‹!");
+//
+//		return 0;
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//
+//	free(p);
+//	p = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int* p = NULL;//∑¿÷π≥…Œ™“∞÷∏’Î
+//	int i = 0;
+//
+//	int* ptr = (int*)malloc(NULL);//…Í«Î40∏ˆ◊÷Ω⁄
+//	if (ptr != NULL)//≈–∂œ «∑Ò…˘«Î≥…π¶
+//	{
+//		p = ptr;
+//	}
+//	else
+//	{
+//		printf("…Í«Îƒ⁄¥Ê ß∞‹!");
+//		return 0;
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//
+//	return 0;
+//}
