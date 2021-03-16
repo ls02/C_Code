2e2bc3b533109b0285c666976b2d352df3be9659
z189 <2877455773@qq.com>
2021-03-16 13:17:41 +0800

    2021

---
+++ contacts1.1.1/test-3-11-2/Contacts.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#define NAME 20
+#define ADDRESS 50
+#define MAX 100
+
+enum  option
+{
+	EXIT,
+	ADD,
+	DEL,
+	REVISE,
+	FIND,
+	ALLDEL,
+	SHOW,
+	SORT
+};
+
+enum optionDel
+{
+	RETURN,
+	SELECTDEL,
+	TAIDEL,
+	HEADDEL
+};
+
+enum  optionRevise
+{
+	REVISENAME = 1,
+	REVISESEX,
+	REVISEAGE,
+	REVISENUMBER,
+	REVISEADDRESS,
+	REVISEALL
+};
+
+enum optionSort
+{
+	NAMESORT = 1,
+	AGESORT,
+	SEXSORT
+};
+
+typedef struct information//通讯录信息
+{
+	char name[NAME];
+	char sex[6];
+	short age;
+	int number;
+	char address[ADDRESS];
+}information;
+
+typedef struct Contacts//通讯录
+{
+	information data[MAX];
+	size_t size;
+}Contacts;
+
+void ContactsPush(Contacts* pc);//添加联系人
+
+void ContactsInitialize(Contacts* pc);// 初始化
+
+void ContactsInsertErase(Contacts* pc, size_t num);//指定位置删除联系人
+
+void ContactsPopFront(Contacts* pc);//删除末尾的联系人
+
+void ContactsPopHead(Contacts* pc);//删除开头的联系人
+
+Contacts* ContactsFind(const Contacts* pc, char* name);//查找
+
+void Contactsrevise(Contacts* pc);//修改
+
+void ContactsAllBrase(Contacts* pc);//删除所有联系人
+
+void* ContactsBubblesort(void* base, size_t num, size_t width, int (compare)(const void* e1, const void* e2));//排序
+
+void ContactsPrint(const Contacts* ps);//打印
---
+++ contacts1.1.1/test-3-11-2/test.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "Contacts.h"
+#include <stdio.h>
+
+extern void Menu();
+
+int main()
+{
+	Contacts ps;
+	char name[NAME] = { 0 };
+	int input = 0;
+
+	ContactsInitialize(&ps);// ³õÊ¼»¯
+
+	do
+	{
+		system("cls");
+
+		Menu();
+		scanf("%d", &input);
+
+		switch (input)
+		{
+		case EXIT:
+			printf("exit\n");
+			break;
+		case ADD:
+			ContactsPush(&ps);
+			break;
+		case DEL:
+			ContactsDel(&ps);
+			break;
+		case REVISE:
+			Contactsrevise(&ps);
+			break;
+		case FIND:
+			printf("enter the name:");
+			scanf("%s", name);
+			getchar();
+			Contacts* temp = ContactsFind(&ps, name);
+
+			if (temp)
+			{
+				printf("%-15s\t%-15s\t%-15s\t%-15s\t%-15s\n", "name", "sex", "age", "number", "addess");
+				printf("%-15s\t%-15s\t%-15hd\t%-15d\t%-15s\n",
+					temp->data->name,
+					temp->data->sex,
+					temp->data->age,
+					temp->data->number,
+					temp->data->address);
+				printf("Press Enter to exit\n");
+				getchar();
+			}
+
+			break;
+		case ALLDEL:
+			ContactsAllBrase(&ps);
+			break;
+		case SHOW:
+			system("cls");
+			ContactsPrint(&ps);
+			break;
+		case SORT:
+			ContactsSort(&ps);
+			break;
+		default:
+			printf("select error\n");
+			Sleep(1000);
+			break;
+		}
+
+	} while (input);
+
+	return 0;
+}
