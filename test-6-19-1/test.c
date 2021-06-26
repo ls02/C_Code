17a5b8805501944b8d584ea1d240b19b7fe34d21
z189 <69874715+z189@users.noreply.github.com>
2021-06-26 09:33:48 +0800

    2021

---
+++ test-6-19-1/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	//char a[] = "abc";
+	//char b[] = { 'a', 'b', 'c' };
+	//int lenb = strlen(b);
+
+	//printf("b:%s\nlen:%d\n", b, lenb);
+	printf("\a\n\a\n\a");
+
+	return 0;
+}
